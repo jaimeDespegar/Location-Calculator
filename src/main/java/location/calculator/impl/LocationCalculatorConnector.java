b572bbe2adc3b263bda2519d031c3aa01d117827
jaimeDespegar <jaime.quispe@despegar.com>
2019-04-28 21:08:22 -0300

    commit repo , impl calculator

---
+++ src/main/java/location/calculator/impl/LocationCalculatorConnector.java
@@ -0,0 +1,55 @@
+package location.calculator.impl;
+
+import com.google.common.collect.Lists;
+import model.Coordinate;
+import model.LocationConnector;
+import java.util.List;
+
+public class LocationCalculatorConnector implements LocationConnector {
+
+    @Override
+    public Boolean isAvailable() {
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public Boolean on() {
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public Boolean off() {
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public List<Coordinate> getData(Coordinate arrival, Coordinate departure) {
+        Double arrivalX = arrival.getX();
+        Double arrivalY = arrival.getY();
+        Double departureX = departure.getX();
+        Double departureY = departure.getY();
+
+        List<Coordinate> road = Lists.newArrayList();
+        if(arrivalX < departureX) {
+            for (int i = arrivalX.intValue(); i <= departureX; i++) {
+                road.add(new Coordinate(i, arrivalY.intValue()));
+            }
+        } else {
+            for (int i = arrivalX.intValue(); i >= departureX; i--) {
+                road.add(new Coordinate(i, arrivalY.intValue()));
+            }
+        }
+        road.remove(road.size()-1);
+        if(arrivalY < departureY) {
+            for (int i = arrivalY.intValue(); i <= departureY; i++) {
+                road.add(new Coordinate(departureX.intValue(), i));
+            }
+        } else {
+            for (int i = arrivalY.intValue(); i >= departureY; i--) {
+                road.add(new Coordinate(departureX.intValue(), i));
+            }
+        }
+        return road;
+    }
+
+}
---
+++ src/main/java/location/calculator/impl/Main.java
@@ -0,0 +1,18 @@
+package location.calculator.impl;
+
+import model.Coordinate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocationCalculatorConnector l = new LocationCalculatorConnector();
+        l.getData(new Coordinate(7, 3), new Coordinate(7, 2))
+         .forEach(i -> System.out.println(i.getX()+" - "+i.getY()));
+
+        System.out.println();
+
+        l.getData(new Coordinate(2, 1), new Coordinate(4, 5))
+                .forEach(i -> System.out.println(i.getX()+" - "+i.getY()));
+    }
+
+}
